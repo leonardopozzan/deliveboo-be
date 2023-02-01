5aadf307af10db37a0ad9c21e2b6f5439cb03b66
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2023-02-01 15:15:15 +0100

    first commit

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Usando Vite -->
+    @vite(['resources/js/app.js'])
+</head>
+
+<body>
+    <div class="d-flex">
+        @include('partials.admin.sidebar')
+        <main class="w-100">
+            @include('partials.admin.header')
+            @yield('content')
+        </main>
+    </div>
+</body>
+
+</html>
