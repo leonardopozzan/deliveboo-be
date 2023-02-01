4332df1153b76187b51f312459578cdfd4c218bd
coder-ms <marco.sbrascia@gmail.com>
2023-02-01 18:06:02 +0100

    resturants.php

--- config/restaurants.php
+++ config/restaurants.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'resturants' => [
+    "resturants" => [
         [
             "name" => "Pizzeria Fuori di Zucca",
             "email" => "pizzeriafuoridizucca@gmail.com",
@@ -39,22 +39,205 @@ return [
             "email" => "oldwildwest@gmail.com",
             "image" => "http://via.placeholder.com/640x360",
             "address" => "Via di Gello 1, 56124 Pisa (PI)",
-            "partita_iva" => "IT46469782222",
+            "partita_iva" => "IT46469783333",
             "website" => "www.oldwildwest.it",
             "opening_hours" => "18:00",
             "closing_hours" => "24:00",
-            "phone_number" => "351 8929384",
+            "phone_number" => "3518929384",
         ],
         [
             "name" => "Sollacchero Pisano",
             "email" => "sollaccheropisano@gmail.com",
             "image" => "http://via.placeholder.com/640x360",
             "address" => "Corso Italia 171, 56123 Pisa (PI)",
-            "partita_iva" => "IT46469783333",
+            "partita_iva" => "IT46469784444",
             "website" => "www.sollaccheropisano.it",
             "opening_hours" => "18:00",
             "closing_hours" => "24:00",
-            "phone_number" => "329 1832442",
+            "phone_number" => "3291832442",
+        ],
+
+        [
+            "name" => "Salsa di Peperon y Monton - Specialità iberiche",
+            "email" => "salsadipeperonymonton@gmail.com",
+            "image" => "http://via.placeholder.com/640x360",
+            "address" => "Corso Francia 12, 56123 Pisa (PI)",
+            "partita_iva" => "IT46469785555",
+            "website" => "www.salsadipeperonymonton.it",
+            "opening_hours" => "18:00",
+            "closing_hours" => "24:00",
+            "phone_number" => "3236525768",
+        ],
+        [
+            "name" => "Avocados from Mexico",
+            "email" => "avocadosfrommexico@gmail.com",
+            "image" => "http://via.placeholder.com/640x360",
+            "address" => "Piazza Vittorio Emanuele 119, 56129 Pisa (PI)",
+            "partita_iva" => "IT46469786666",
+            "website" => "www.avocadosfrommexico.it",
+            "opening_hours" => "18:00",
+            "closing_hours" => "24:00",
+            "phone_number" => "39163324523",
+        ],
+        [
+            "name" => "Ludhiana - Punjabi Traditional Cuisine",
+            "email" => "punjabitraditionalcuisine@gmail.com",
+            "image" => "http://via.placeholder.com/640x360",
+            "address" => "Via Leonardo Fibonacci 30, 56123 Pisa (PI)",
+            "partita_iva" => "IT46469787777",
+            "website" => "www.punjabitraditionalcuisine.it",
+            "opening_hours" => "18:00",
+            "closing_hours" => "24:00",
+            "phone_number" => "3456923426",
+        ],
+        [
+            "name" => "Ankara - Turkish Traditional Cuisine",
+            "email" => "ankaratraditionalcuisine@gmail.com",
+            "image" => "http://via.placeholder.com/640x360",
+            "address" => "Via Giacomo Leopardi 64, 56123 Pisa (PI)",
+            "partita_iva" => "IT46469788888",
+            "website" => "www.ankaratraditionalcuisine.it",
+            "opening_hours" => "18:00",
+            "closing_hours" => "24:00",
+            "phone_number" => "3477034537",
+        ],
+        [
+            "name" => "Da Zorba il Greco",
+            "email" => "zorbailgreco@gmail.com",
+            "image" => "http://via.placeholder.com/640x360",
+            "address" => "Via Vittorio Veneto 44, 56121 Pisa (PI)",
+            "partita_iva" => "IT46469780000",
+            "website" => "www.zorbailgreco.it",
+            "opening_hours" => "18:00",
+            "closing_hours" => "24:00",
+            "phone_number" => "3526644883",
+        ],
+        [
+            "name" => "From Urals with Love",
+            "email" => "fromuralswithlove@gmail.com",
+            "image" => "http://via.placeholder.com/640x360",
+            "address" => "Via Vittorio Veneto 44, 56121 Pisa (PI)",
+            "partita_iva" => "IT46469110000",
+            "website" => "www.fromuralswithlove.it",
+            "opening_hours" => "18:00",
+            "closing_hours" => "24:00",
+            "phone_number" => "3486323100",
+        ],
+    ],
+
+    "types" => [ 
+        [
+            "type_name" => "greco",
+            "type_imgPath" => "http://via.placeholder.com/640x360"
+        ],
+        [
+            "type_name" => "russo",
+            "type_imgPath" => "http://via.placeholder.com/640x360"
+        ],
+        [
+            "type_name" => "pizzeria",
+            "type_imgPath" => "http://via.placeholder.com/640x360"
+        ],
+        [
+            "type_name" => "italiano",
+            "type_imgPath" => "http://via.placeholder.com/640x360"
+        ],
+        [
+            "type_name" => "amricano",
+            "type_imgPath" => "http://via.placeholder.com/640x360"
+        ],
+        [
+            "type_name" => "cinese",
+            "type_imgPath" => "http://via.placeholder.com/640x360"
+        ],
+        [
+            "type_name" => "giapponese",
+            "type_imgPath" => "http://via.placeholder.com/640x360"
+        ],
+        [
+            "type_name" => "spagnolo",
+            "type_imgPath" => "http://via.placeholder.com/640x360"
+        ],
+        [
+            "type_name" => "turco",
+            "type_imgPath" => "http://via.placeholder.com/640x360"
+        ],
+        [
+            "type_name" => "indiano",
+            "type_imgPath" => "http://via.placeholder.com/640x360"
+        ],
+        [
+            "type_name" => "messicano",
+            "type_imgPath" => "http://via.placeholder.com/640x360"
+        ],
+        [
+            "type_name" => "vegana",
+            "type_imgPath" => "http://via.placeholder.com/640x360"
+        ],
+        [
+            "type_name" => "vegetariana",
+            "type_imgPath" => "http://via.placeholder.com/640x360"
+        ],
+        [
+            "type_name" => "celiaca",
+            "type_imgPath" => "http://via.placeholder.com/640x360"
+        ],
+    ],
+    "reviews" => [ 
+        [
+            "rev_name" => "Gioffre Ambrogetti",
+            "rev_rating" => 9.0,
+            "rev_message" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique temporibus eligendi deleniti libero quae sed saepe, obcaecati nulla quidem excepturi."
+        ],
+        [
+            "rev_name" => "Rob Stowell",
+            "rev_rating" => 4.5,
+            "rev_message" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique temporibus eligendi deleniti libero quae sed saepe, obcaecati nulla quidem excepturi."
+        ],
+        [
+            "rev_name" => "Valeria Boffi",
+            "rev_rating" => 6.5,
+            "rev_message" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique temporibus eligendi deleniti libero quae sed saepe, obcaecati nulla quidem excepturi."
+        ],
+        [
+            "rev_name" => "Maurizio Magrini",
+            "rev_rating" => 9.5,
+            "rev_message" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique temporibus eligendi deleniti libero quae sed saepe, obcaecati nulla quidem excepturi."
+        ],
+        [
+            "rev_name" => "Stefania Tombessi",
+            "rev_rating" => 3.5,
+            "rev_message" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique temporibus eligendi deleniti libero quae sed saepe, obcaecati nulla quidem excepturi."
+        ],
+        [
+            "rev_name" => "Nicoletta Frontini",
+            "rev_rating" => 8.5,
+            "rev_message" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique temporibus eligendi deleniti libero quae sed saepe, obcaecati nulla quidem excepturi."
+        ],
+        [
+            "rev_name" => "Davide Fratangelo",
+            "rev_rating" => 7.5,
+            "rev_message" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique temporibus eligendi deleniti libero quae sed saepe, obcaecati nulla quidem excepturi."
+        ],
+        [
+            "rev_name" => "Adelmo Morandi",
+            "rev_rating" => 7.0,
+            "rev_message" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique temporibus eligendi deleniti libero quae sed saepe, obcaecati nulla quidem excepturi."
+        ],
+        [
+            "rev_name" => "Haroun Lucini",
+            "rev_rating" => 8.0,
+            "rev_message" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique temporibus eligendi deleniti libero quae sed saepe, obcaecati nulla quidem excepturi."
+        ],
+        [
+            "rev_name" => "Michael Allison",
+            "rev_rating" => 6.5,
+            "rev_message" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique temporibus eligendi deleniti libero quae sed saepe, obcaecati nulla quidem excepturi."
+        ],
+        [
+            "rev_name" => "Guglielmina Morini",
+            "rev_rating" => 5.5,
+            "rev_message" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique temporibus eligendi deleniti libero quae sed saepe, obcaecati nulla quidem excepturi."
         ],
     ],
 ];
