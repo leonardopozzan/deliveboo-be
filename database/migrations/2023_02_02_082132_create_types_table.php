6e158597dea5dd3ccec231a8c602fa9af88d9623
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2023-02-02 21:23:41 +0100

    fix migration

--- database/migrations/2023_02_02_082132_create_types_table.php
+++ database/migrations/2023_02_02_082132_create_types_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50)->unique();
+            $table->string('slug', 100);
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
