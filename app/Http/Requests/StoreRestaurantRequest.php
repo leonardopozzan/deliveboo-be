e0363aa8b0c536308aa9e40933e74d00a6164081
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2023-02-07 00:17:42 +0100

    create a restaurant

--- app/Http/Requests/StoreRestaurantRequest.php
+++ app/Http/Requests/StoreRestaurantRequest.php
@@ -13,7 +13,7 @@ class StoreRestaurantRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,50 @@ class StoreRestaurantRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' =>'required|unique:restaurants|max:100|min:3',
+            'email' =>'required|unique:restaurants|email|max:100|min:3',
+            'address' =>'required|max:255|min:3',
+            'p_iva' =>'required|size:13|regex:/^([I,T]){2}([0-9]){11}$/|starts_with:IT',
+            'website' => 'nullable|url',
+            'opening_hours' => 'required|date_format:H:i',
+            'closing_hours' => 'required|date_format:H:i',
+            'phone_number' => 'required|regex:/^([0-9]*)$/|size:10'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Il nome è obbligatorio.',
+            'name.min' => 'Il nome deve essere lungo almeno :min caratteri.',
+            'name.max' => 'Il nome non può superare i :max caratteri.',
+            'name.unique:restaurants' => 'Il nome esiste già',
+
+            'email.required' => 'L\'email è obbligatoria.',
+            'email.min' => 'L\'email deve essere lunga almeno :min caratteri.',
+            'email.max' => 'L\'email non può superare i :max caratteri.',
+            'email.unique:restaurants' => 'L\'email esiste già',
+
+            'address.required' => 'L\'indirizzo è obbligatorio.',
+            'address.min' => 'L\'indirizzo deve essere lungo almeno :min caratteri.',
+            'address.max' => 'L\'indirizzo non può superare i :max caratteri.',
+
+            'p_iva.required' => 'La partita iva è obbligatoria.',
+            'p_iva.size' => 'La partita iva deve essere lunga esattamente :size caratteri.',
+            'p_iva.regex' => 'La partita deve essere scritta come IT99999999999.',
+            'p_iva.starts_with' => 'La partita iva deve iniziare con IT.',
+
+            'website.url' => 'Il sito web deve essere un url.',
+
+            'opening_hours.required' => 'L\'orario di apertura è obbligatorio.',
+            'opening_hours.date_format' => 'L\'orario di apertura deve essere nel formato hh:mm.',
+
+            'closing_hours.required' => 'L\'orario di apertura è obbligatorio.',
+            'closing_hours.date_format' => 'L\'orario di apertura deve essere nel formato hh:mm.',
+
+            'phone_number.required' => 'Il numero di telefono è obbligatorio.',
+            'phone_number.size' => 'Il numero di telefono deve essere lungo esattamente :size caratteri.',
+            'phone_number.regex' => 'Il numero di telefono può contenere solo numeri.',
         ];
     }
 }
--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -1,5 +1,106 @@
 @extends('layouts.admin')
 @section('content')
-<h1>create</h1>
-
+<section class="container my-5" id="create-restaurant">
+    <h1 class="mb-4">Crea il tuo ristorante</h1>
+        <div class="row bg-white">
+            <div class="col-12">
+                <form action="{{ route('admin.restaurants.store') }}" method="POST" enctype="multipart/form-data" class="form-crud">
+                    @csrf
+                    {{-- Nome Ristorante --}}
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Nome<span>*</span></label>
+                        <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" required maxlength="100" minlength="3" value="{{old('name')}}">
+                        @error('name')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    {{-- Email Ristorante --}}
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email <span>*</span></label>
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" required maxlength="100" minlength="3" value="{{old('email')}}">
+                        @error('email')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    {{-- Partita iva Ristorante --}}
+                    <div class="mb-3">
+                        <label for="p_iva" class="form-label">Partita IVA <span>*</span></label>
+                        <input type="text" name="p_iva" id="p_iva" class="form-control  @error('p_iva') is-invalid @enderror" required maxlength="13" minlength="13" value="{{old('p_iva')}}">
+                        @error('p_iva')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    {{-- Indirizzo Ristorante --}}
+                    <div class="mb-3">
+                        <label for="address" class="form-label">Indirizzo <span>*</span></label>
+                        <input type="text" name="address" id="address" class="form-control  @error('address') is-invalid @enderror" required maxlength="255" minlength="3" value="{{old('address')}}">
+                        @error('address')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    {{-- Numero di telefono del Ristorante --}}
+                    <div class="mb-3">
+                        <label for="phone_number" class="form-label">Numero di Telefono <span>*</span></label>
+                        <input type="text" name="phone_number" id="phone_number" class="form-control  @error('phone_number') is-invalid @enderror" required maxlength="10" minlength="10" value="{{old('phone_number')}}">
+                        @error('phone_number')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    {{-- Orari di apertura --}}
+                    <div class="mb-3 d-flex align-items-start">
+                        <div class="me-4">
+                            <label for="opening_hours" class="form-label">Orario di apertura <span>*</span></label>
+                            <input type="time" name="opening_hours" id="opening_hours" class="form-control  @error('opening_hours') is-invalid @enderror" required value="{{old('opening_hours')}}">
+                            @error('opening_hours')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div>
+                            <label for="closing_hours" class="form-label">Orario di chiusura <span>*</span></label>
+                            <input type="time" name="closing_hours" id="closing_hours" class="form-control  @error('closing_hours') is-invalid @enderror" required value="{{old('closing_hours')}}">
+                            @error('closing_hours')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                    {{-- Tipo di  Ristorante --}}
+                    <div class="mb-3 row">
+                    <label for="types" class="form-label">Tipo <span>*</span></label>
+                        @foreach ($types as $type)
+                            @if (old("types"))
+                            <div class="d-flex col-xxl-2 col-lg-3 col-md-4 col-6">
+                                <input class="me-2" type="checkbox" name="types[]" value="{{ $type->id }}" {{in_array( $type->id, old("types", []) ) ? 'checked' : ''}}>
+                                <span class="text-capitalize">{{ $type->name }}</span>
+                            </div>
+                            @else
+                                <div class="d-flex col-xxl-2 col-lg-3 col-md-4 col-6">
+                                    <input class="me-2" type="checkbox" name="types[]" value="{{ $type->id }} " {{ old('types') ? (old('types')->contains($type->id) ? 'checked' : '') : '' }}>
+                                    <span class="text-capitalize">{{ $type->name }}</span>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div> 
+                    {{-- Sito web del Ristorante --}}
+                    <div class="mb-3">
+                        <label for="website" class="form-label">Sito web</label>
+                        <input type="text" name="website" id="website" class="form-control  @error('website') is-invalid @enderror" value="{{old('website')}}">
+                        @error('website')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    {{-- Immagine Ristorante --}}
+                    <div >
+                        <label for="image" class="form-label">Immagine</label>
+                        <input type="file" name="image" id="create_cover_image" class="form-control  @error('image') is-invalid @enderror">
+                        @error('image')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-4 mt-1">*campi obbligatori</div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="reset" class="btn btn-danger">Reset</button>
+                </form>
+            </div>
+        </div>
+    </section>
 @endsection
