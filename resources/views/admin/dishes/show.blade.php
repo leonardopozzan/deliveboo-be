73f0ba7b6f5ebf31be50ae3a19376a797005851e
coder-ms <marco.sbrascia@gmail.com>
2023-02-07 19:32:12 +0100

    show dish aggiornato

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -1,29 +1,39 @@
 @extends('layouts.admin')
 @section('content')
-<h1 class="text-center">{{$dish->name}}</h1>
-<div class="containerx">
-    <div class="cardShow">
-        <div class="cardImageShow">
-            {{-- {{dd($dish)}}  --}}
-            <img src="{{asset('storage/' . $dish->image)}}" alt="">
-        </div>
-          <div class="cardDescriptionShow">
-            <p>Ingredienti : {{$dish->ingredients}}</p> 
-            <p>Prezzo : {{$dish->price}}</p> 
-            @if ($dish->category)
-                <p>Categoria : {{$dish->category->name}}</p>
-            @endif
 
-            <p>Visibile : {{$dish->visible}}</p>
-            
-            <a href="{{route('admin.dishes.index', $dish->slug)}}" class="btn btn-primary">INDIETRO</a>
-            <a href="{{route('admin.dishes.edit', $dish->slug)}}" class="btn btn-secondary">MODIFICA</a>
-            <form action="{{route('admin.dishes.destroy', $dish->slug)}}" method="POST" class=" d-inline">
-              @csrf
-              @method('DELETE')
-              <button type="submit" class="btn btn-danger">ELIMINA</button>
-            </form>
+
+<body>
+  <div class="containerShow">
+      <div class="titleDish text-center">
+          <h1 class="text-center text-uppercase">{{$dish->name}}</h1>
+      </div>
+      <div class="containery">
+          <div class="cardShow">
+              <div class="cardImageShow">
+                  <img src="{{asset('storage/' . $dish->image)}}" alt="">
+              </div>
+              <div class="cardDescriptionShow p-5">
+                  <p>Ingredienti : {{$dish->ingredients}}</p> 
+                  <p>Prezzo : {{$dish->price}}</p> 
+                  @if ($dish->category)
+                      <p>Categoria : {{$dish->category->name}}</p>
+                  @endif
+        
+                  <p>Visibile : {{$dish->visible}}</p>
+               </div>
           </div>
-    </div>
-</div> 
+      </div>
+  
+      <div class="d-flex justify-content-around m-4">
+        <a href="{{route('admin.dishes.index', $dish->slug)}}" class="btn btn-primary">INDIETRO</a>
+        <a href="{{route('admin.dishes.edit', $dish->slug)}}" class="btn btn-secondary">MODIFICA</a>
+        <form action="{{route('admin.dishes.destroy', $dish->slug)}}" method="POST" class=" d-inline">
+          @csrf
+          @method('DELETE')
+          <button type="submit" class="btn btn-danger">ELIMINA</button>
+        </form>
+      </div> 
+  </div>
+</body>
+
 @endsection
