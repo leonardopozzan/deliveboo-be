1add9f195cdad1ee879cf6ff499f0bec1bba49ed
coder-ms <marco.sbrascia@gmail.com>
2023-02-13 12:25:41 +0100

    fix home page back end

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -31,10 +31,4 @@
 
     </div>
 </div>
-
-<div class="content">
-    {{-- <div class="container">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis accusamus dolores!</p>
-    </div> --}}
-</div>
 @endsection
