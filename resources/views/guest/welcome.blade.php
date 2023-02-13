6a53a48ef8508ecec9f636c6581d37e460e3a73a
coder-ms <marco.sbrascia@gmail.com>
2023-02-13 10:45:49 +0100

    add logo on navbar

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <div class="jumbotron p-5 mb-4 bg-light rounded-3">
-    <div class="container py-5">
+    {{-- <div class="container py-5">
         <div class="logo_laravel">
             <svg viewBox="0 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-25">
                 <g clip-path="url(#clip0)" fill="#EF3B2D">
@@ -16,12 +16,12 @@
 
         <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
         <button class="btn btn-primary btn-lg" type="button">Example button</button>
-    </div>
+    </div> --}}
 </div>
 
 <div class="content">
-    <div class="container">
+    {{-- <div class="container">
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis accusamus dolores!</p>
-    </div>
+    </div> --}}
 </div>
 @endsection
