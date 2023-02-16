df99dca39aca39acac41eac9e16489c794816ade
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2023-02-16 15:25:00 +0100

    inserted mail to restaurant & customer

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -4,15 +4,37 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\OrderPaymentRequest;
+use App\Mail\NewOrder;
 use App\Models\Dish;
+use App\Models\Lead;
 use App\Models\Order;
+use App\Models\Restaurant;
 use Braintree\Gateway;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+
 use Faker\Generator as Faker;
 
 class CartController extends Controller
 {
     public function purchase(Request $request, Faker $faker){
+        
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name' => 'required|max:100',
+            'email' => 'required|email|max:100',
+            'address' => 'required|max:150',
+            'phoneNumber' => 'required|regex:/^([0-9]*)$/|max:10',
+        ],);
+
+        if($validator->fails()){
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]); 
+        }
 
         $new_order = new Order();
         $today = date("Ymd");
@@ -36,6 +58,21 @@ class CartController extends Controller
             $list_item[$key] = ['quantity' => $quantity , 'current_price' => $price];
         }
         $new_order->dishes()->attach($list_item);
+
+
+
+        $new_lead = new Lead();
+        $new_lead->name = $data['name'];
+        $new_lead->email = $data['email'];
+        $new_lead->message = $new_order->code;
+        $new_lead->save();
+
+        Mail::to($new_lead->email)->send(new NewOrder($new_lead));
+
+        $restaurant_email = Restaurant::where('id',$request->cart[0]['restaurant_id'])->first()->email;
+        
+        Mail::to($restaurant_email)->send(new NewOrder($new_lead));
+
 
         return response()->json([
             'results' => $request->all(),
